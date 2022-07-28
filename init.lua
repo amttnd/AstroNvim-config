ebf7da777086e246a9231c6df9d53122cfafcc36
tam-nd <ndtamhcmus@gmail.com>
2022-07-28 18:49:56 +0700

    chore: feline

--- init.lua
+++ init.lua
@@ -2,7 +2,20 @@ local config = {
   plugins = {
     init = {
       ["wakatime/vim-wakatime"] = {},
-    }
+    },
+    ["feline"] = function(config)
+      local status_ok, feline = pcall(require, "feline.providers.vi_mode")
+      if not status_ok then return config end
+      local C = require "default_theme.colors"
+      local hl = require("core.status").hl
+      config.components.active[1][1] = {
+        provider = function()
+          return ' '..feline.get_vim_mode()..' '
+        end,
+        hl = hl.mode()
+      }
+      return config
+    end
   },
 
   mappings = {
